9dc194498ad2dd11f468fff099173f474899a870
Ruso <Ruso>
2019-09-11 20:50:09 -0300

    Added functional About page

--- apps/green_planet_frontend/urls.py
+++ apps/green_planet_frontend/urls.py
@@ -1,7 +1,9 @@
 """ Module green_planet_backend REST URL Configuration """
 from django.urls import path
 from .views import IndexView
+from .views import AboutView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name="index view"),
+    path('', IndexView.as_view(), name="index_view"),
+    path('about', AboutView.as_view(), name="about_view"),
 ]
--- apps/green_planet_frontend/views.py
+++ apps/green_planet_frontend/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     """ Main Index view - redirects to one page template """
     template_name = "green_planet_frontend/index.html"
+
+class AboutView(TemplateView):
+    """ Main Index view - redirects to one page template """
+    template_name = "green_planet_frontend/about.html"
