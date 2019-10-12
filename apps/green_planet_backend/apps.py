f5b366721312be4abde5d648bd2157bcc9c11f18
svsrus <sergei.shurpenkov@yandex.ru>
2019-10-12 17:27:15 -0300

    Added verbose name to backend config module

--- apps/green_planet_backend/apps.py
+++ apps/green_planet_backend/apps.py
@@ -1,5 +1,7 @@
+""" Green Planet Application configuration """
 from django.apps import AppConfig
 
-
 class GreenPlanetBackendConfig(AppConfig):
+    """ Green Planet Backend module configuration class """
     name = 'green_planet_backend'
+    verbose_name = 'Green Planet Backend module'
