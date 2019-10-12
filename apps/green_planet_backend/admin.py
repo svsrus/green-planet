722a907dadcaf6276ea8fd7d87d1f825c2c208de
svsrus <sergei.shurpenkov@yandex.ru>
2019-10-12 17:26:53 -0300

    Added entity administration for all backend models

--- apps/green_planet_backend/admin.py
+++ apps/green_planet_backend/admin.py
@@ -1,3 +1,7 @@
+""" Django Admin configurations, registers all entities. """
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+app = apps.get_app_config('green_planet_backend')
+for model_name, model in app.models.items():
+    admin.site.register(model)
