0c9c2794da58fa1aa5575130ba107ac8fab28738
svsrus <sergei.shurpenkov@yandex.ru>
2019-10-12 17:29:32 -0300

    Pythondoc changes

--- apps/green_planet_frontend/views.py
+++ apps/green_planet_frontend/views.py
@@ -2,9 +2,9 @@
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    """ Main Index view - redirects to one page template """
+    """ Main Index view - redirects to main page template """
     template_name = "green_planet_frontend/index.html"
 
 class AboutView(TemplateView):
-    """ Main Index view - redirects to one page template """
+    """ About view - redirects to page template """
     template_name = "green_planet_frontend/about.html"
