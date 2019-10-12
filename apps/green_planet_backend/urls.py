c0d24ac90192211ee8f4b4fe9a0471b3039c3823
svsrus <sergei.shurpenkov@yandex.ru>
2019-10-12 17:29:11 -0300

    Added article related urls and views

--- apps/green_planet_backend/urls.py
+++ apps/green_planet_backend/urls.py
@@ -1,7 +1,10 @@
 """ Module green_planet_backend REST URL Configuration """
 from django.urls import path
-from .views import TestView
+from .views import ArticleView
+from .views import LatestArticlesView
 
 urlpatterns = [
-    path('test', TestView.as_view(), name="test view"),
+    path('latestArticles/', LatestArticlesView.as_view(), name="latest_articles"),
+    path('articles/<int:article_id>', ArticleView.as_view(), name="articles"),
+    path('articles/', ArticleView.as_view(), name="articles"),
 ]
--- apps/green_planet_backend/views.py
+++ apps/green_planet_backend/views.py
@@ -1,8 +1,26 @@
 """ GREEN PLANET BACKEND - views module """
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-class TestView(APIView):
+from .models import Article
+from .models_serializers import ArticleSerializer
+
+class LatestArticlesView(APIView):
+    """ View Class is responsible for delivering latest articles """
+
     def get(self, request):
-        json = [{"first_name":"Sergei"}, {"first_name":"Olga"}, {"first_name":"Katia"}, {"first_name":"Nastia"}]
-        return Response(json)
+        """ Method searches lastest published articles and returns this list as JSON """
+        articles = Article.objects.filter().order_by('-article_id')[:3]
+        article_serializer = ArticleSerializer(articles, many=True)
+        articles_json = article_serializer.data
+        return Response(articles_json)
+
+class ArticleView(APIView):
+    """ View Class is responsible for managing article entity """
+
+    def get(self, request, article_id=None):
+        """ Method searches article by a given id in request, and returns it as JSON """
+        article = Article.objects.get(pk=article_id)
+        article_serializer = ArticleSerializer(article, many=False)
+        articles_json = article_serializer.data
+        return Response(articles_json)
