e3e5d836762646d17bfcd6be5ccd65b4366bc978
svsrus <sergei.shurpenkov@yandex.ru>
2019-10-23 16:37:00 -0300

    Added MEDIA_URL and MEDIA_ROOT and URL for DEVENV

--- green_planet/urls.py
+++ green_planet/urls.py
@@ -1,4 +1,4 @@
-"""green_planet URL Configuration
+""" Green_planet MAIN URL Configurations
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
@@ -22,4 +24,4 @@ urlpatterns = [
     #path('', include('apps.green_planet_frontend_react.urls')),
     path('api/', include('apps.green_planet_backend.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
