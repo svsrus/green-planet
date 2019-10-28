f7086366a5d881b2d487409a8f256aaa715400a1
svsrus <sergei.shurpenkov@yandex.ru>
2019-10-28 15:06:58 -0300

    Separation URL of PUT method for article and articleRepresentations

--- apps/green_planet_backend/urls.py
+++ apps/green_planet_backend/urls.py
@@ -1,10 +1,12 @@
 """ Module green_planet_backend Article REST URL Configuration with MEDIA_ROOT for debugging """
 from django.urls import path
 from .views import ArticleView
+from .views import ArticleRepresentationView
 from .views import LatestArticlesView
 
 urlpatterns = [
     path('latestArticles/', LatestArticlesView.as_view(), name="latest_articles"),
     path('articles/<int:article_id>', ArticleView.as_view(), name="articles"),
     path('articles/', ArticleView.as_view(), name="articles"),
+    path('articleRepresentations/', ArticleRepresentationView.as_view(), name="article_representations"),
 ]
