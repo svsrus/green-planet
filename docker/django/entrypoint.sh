97ba1c0eb60f94652009dba9f9cc3ff8805bbf9f
svsrus <sergei.shurpenkov@yandex.ru>
2019-11-25 17:55:53 -0300

    Functional docker compose for nginx, gunicorn, postgres and app

---
+++ docker/django/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo 'Executing entrypoint.sh for django database migration.'
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+    sleep 10
+    
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      echo "Sleeping for 10 seconds..."
+      sleep 10
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py migrate --no-input
+
+exec "$@"
