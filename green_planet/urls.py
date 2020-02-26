afd3f72c16dfb1dbb68a5599662a59b6ddf32851
svsrus <sergei.shurpenkov@yandex.ru>
2020-02-26 16:43:02 -0300

    Added i18n URL mapping for language selection

--- green_planet/urls.py
+++ green_planet/urls.py
@@ -18,10 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf.urls import url
 
 urlpatterns = [
     path('', include('apps.green_planet_frontend.urls')),
     #path('', include('apps.green_planet_frontend_react.urls')),
     path('api/', include('apps.green_planet_backend.urls')),
     path('admin/', admin.site.urls),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
